73ef16936874ff03820a632ff9f3891b12af973c
Liewona <40515807+Liewona@users.noreply.github.com>
2020-10-29 19:23:34 +0800

    first commit

---
+++ src/components/WangEditor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :id="id" style="width:800px; height: 400px">
+     <p>欢迎使用 <b>wangEditor</b> 富文本编辑器</p>
+  </div>
+</template>
+
+<script>
+import E from 'wangeditor';
+
+export default {
+  name: 'wangeditor',
+  props: {
+    id: String
+  },
+  mounted() {
+    if(this.id == undefined) {
+      this.id = '123';
+    }
+    this.editor = new E(`#${this.id}`);
+    this.editor.config.uploadImgServer = '/upload-img'
+    this.editor.create();
+  },
+  data() {
+    return {
+      editor: Object,
+    }
+  },
+  methods: {
+    getVal() {
+      console.log(this.editor.txt.text());
+      console.log(this.editor.txt.html());
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <wang-editor :id="'test'" ref="test"></wang-editor>
+    <button @click="getVal">点击我胸襟送</button>
+
+    <hr>
+    <div v-html="editorContent">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import WangEditor from "@/components/WangEditor.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld, WangEditor
+  },
+  data() {
+    return {
+      editorContent: String
+    }
+  },
+  methods: {
+    getVal() {
+      console.log(this.$refs);
+      this.$refs.test.getVal();
+      this.editorContent = this.$refs.test.editor.txt.html();
+    }
+  }
+};
+</script>
